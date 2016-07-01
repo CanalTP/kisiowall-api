5fa67542ba80b0191c9c1da0e40014c30c5a0348
MatthioFishal <gariel.matthieu@hotmail.fr>
2016-07-01 12:15:38 +0200

    Add files via upload
    /downloads_by_store requests added but with raw text, waiting for cache to be set up

--- kisiowall-api.py
+++ kisiowall-api.py
@@ -5,6 +5,7 @@ import logging
 import requests
 import yaml
 import pytz
+import redis
 
 
 CONFIGURATION_FILE = "kisiowall-api.yaml"
@@ -96,6 +97,51 @@ def get_volume_errors():
 
     return content, status_code
 
+def make_request(uri, **querystring_params):
+  headers = {"X-Client-Key": config['apikey_appfigures']}
+  auth =(config['username_appfigures'], config['password_appfigures'])
+  return requests.get(config['url_appfigures'] + uri.lstrip("/"),
+                        auth=auth,
+                        params=querystring_params,
+                        headers=headers)
+
+
+@app_api.route("/downloads_by_store")
+def get_downloads_by_store():
+    """
+    Get number of downloads of all our apps each day.
+    :return: json
+    """
+    content = None
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    content =  {'google_play': '233875' , 'ios_store': '491488' }
+
+
+    """
+    try:
+        if (datetime.now - timedelta(hours=1)) > last_cache_update:
+            try:
+                dl_response = make_request("/reports/sales/?group_by=store")
+
+                if dl_response.status_code == 200:
+                    content =  {'google_play': dl_response.json()['google_play']['downloads'],
+                             'ios_store': dl_response.json()['apple:ios']['downloads']}
+                    status_code = status.HTTP_200_OK
+                    cache_connexion = redis.Redis(connection_pool=pool)
+                    cache_connexion.set('dlbyapp', content)
+                    last_cache_update = datetime.now()
+            except Exception as e:
+                content = str(e)
+        else:
+            cache_connexion = redis.Redis(connection_pool=pool)
+            content = cache_connexion.get('dlbyapp')
+    except Exception as e:
+        content = str(e)
+    """
+
+    return content, status_code
+
 
 def app_logging(log_file, lvl=logging.INFO):
     """
@@ -123,5 +169,9 @@ if __name__ == "__main__":
     # Enable logs
     app_logging(config['log_file'])
 
+    # Set connection to Redis cache db
+    """pool = redis.ConnectionPool(host=config['host_relis'], port=config['port_relis'], db=0)
+    last_cache_update = datetime.now()"""
+
     # Create access log for api call
-    app_api.run(port=config['port'])
+app_api.run(port=config['port'])
